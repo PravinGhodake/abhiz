f1002f03cf3ad7263f037829f336da4396ddb115
PravinGhodake <77071150+PravinGhodake@users.noreply.github.com>
2021-02-13 11:34:16 +0530

    Update Demo1.java
     A d d e d   C o m m e n t

--- src/Demo1.java
+++ src/Demo1.java
@@ -1,7 +1,8 @@
 
 public class Demo1 {
 	public static void main(String[] args) {
-		System.out.println("HI EEVERYONE");
+		System.out.println("HI EEVERYONE"); 
+		//   H i i    A  b  h i z   t h i s   i s   s i m  p l e   j a v a    p r i n t i n g   p r o g r a  m
 	}
 
 }
